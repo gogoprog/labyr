aa4ff542901b73bb93307808c775be3a7fd68b56
Gauthier Billot <gauthier.billot@fishingcactus.com>
2016-07-02 17:12:42 +0200

    Added MenuSystem

--- src/Application.hx
+++ src/Application.hx
@@ -3,20 +3,36 @@ import gengine.math.*;
 import gengine.components.*;
 import gengine.graphics.*;
 
+import ash.fsm.*;
+
+import systems.*;
+
 class Application
 {
     private static var engine:Engine;
+    public static var esm:EngineStateMachine;
 
     public static function init()
     {
         trace("Labyr init");
         Gengine.setWindowSize(new IntVector2(1024, 768));
         Gengine.setWindowTitle("Labyr");
-        //Gengine.setGuiFilename("gui/gui.html");
+        Gengine.setGuiFilename("gui/gui.html");
     }
 
     public static function start(_engine:Engine)
     {
         engine = _engine;
+
+        esm = new EngineStateMachine(engine);
+
+        var menuState = new EngineState();
+        menuState.addInstance(new MenuSystem());
+        esm.addState("Menu", menuState);
+
+        var gameState = new EngineState();
+        esm.addState("Game", gameState);
+
+        esm.changeState("Menu");
     }
 }
---
+++ src/systems/MenuSystem.hx
@@ -0,0 +1,52 @@
+package systems;
+
+import gengine.*;
+import ash.systems.*;
+import js.*;
+
+@:expose('Menu')
+class MenuSystem extends System
+{
+    private var engine:Engine;
+
+    public function new()
+    {
+        super();
+        init();
+    }
+
+    public function init()
+    {
+        new JQuery(".startButton").click(onStartClick);
+        new JQuery(".quitButton").click(onQuitClick);
+    }
+
+    override public function addToEngine(_engine:Engine)
+    {
+        engine = _engine;
+    }
+
+    override public function update(dt:Float):Void
+    {
+        var input = Gengine.getInput();
+
+        if(input.getScancodePress(41))
+        {
+            trace("Labyr exit.");
+            Gengine.exit();
+        }
+    }
+
+    private function onStartClick()
+    {
+        trace("onStartClick");
+
+        Application.esm.changeState("Game");
+    }
+
+    private function onQuitClick()
+    {
+        trace("Labyr exit.");
+        Gengine.exit();
+    }
+}
